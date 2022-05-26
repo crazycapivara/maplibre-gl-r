cf93133ea59d4e838dbea2a4acd05ae976d65b65
fry <fry@futurama.de>
2022-05-26 13:06:51 +0200

    Switch to maplibregl-js 2.1.9

--- inst/htmlwidgets/lib/maplibre-gl/get.sh
+++ inst/htmlwidgets/lib/maplibre-gl/get.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
-wget https://unpkg.com/maplibre-gl@1.14.0-rc.1/dist/maplibre-gl.js
-wget https://unpkg.com/maplibre-gl@1.14.0-rc.1/dist/maplibre-gl.css
+VERSION=2.1.9
+#wget https://unpkg.com/maplibre-gl@1.14.0-rc.1/dist/maplibre-gl.js
+#wget https://unpkg.com/maplibre-gl@1.14.0-rc.1/dist/maplibre-gl.css
 
+wget https://unpkg.com/maplibre-gl@${VERSION}/dist/maplibre-gl.js
+wget https://unpkg.com/maplibre-gl@${VERSION}/dist/maplibre-gl.css
